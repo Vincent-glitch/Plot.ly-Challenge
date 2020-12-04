2ffdda83d58278a3977d65f3834bda0dd28330e0
Vincent-glitch <66323103+Vincent-glitch@users.noreply.github.com>
2020-12-04 14:33:32 -0500

    cleaner

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this assignment, you will build an interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.',
+    author='Vincent D'Alto',
+    license='MIT',
+)
